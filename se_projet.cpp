52c2a1f0266a43d510500cd3b939480967ced6a9
islamouahab <islamouahab14@gmail.com>
2023-04-08 09:03:31 +0100

    project done

--- se_projet.cpp
+++ se_projet.cpp
@@ -3,13 +3,17 @@
 #include <time.h>
 #include <windows.h>
 #include <unistd.h>
+#include <vector>
+#include <array>
+#include <stdlib.h>
 using namespace std;
 using std::string;
 
-int init_batch(void);
+void init_batch(void);
+void init_TP(void);
 class node{
     public:
-    int num_pross,tmp_att,tmp_exe,tmp_sort,id,tmp_perdu;
+    int num_pross,tmp_att,tmp_exe,tmp_sort,id,tmp_perdu,tmp_exe_tmp;
     time_t tmp_sec;
     struct tm tmp_arr;
     string etat;
@@ -22,6 +26,7 @@ class node{
         this->num_pross=1+(rand()%100);
         this->etat="P";
         this->tmp_exe=unit_calcul;
+        this->tmp_exe_tmp=unit_calcul;
         this->tmp_perdu=0;
         this->tmp_sec=t;
     }
@@ -46,8 +51,18 @@ class file{
         this->tail=t;
     }
 };
-
-
+struct tab{
+    vector<string> s;
+};
+tab arr[20];
+int nbr_node;
+bool isFinished(node *rem){
+    if (rem->tmp_exe==0){
+        rem->etat="T";
+        return true;
+    }
+    return false;
+}
 void enfiler(node *n,file *f){
            if (f->head==NULL && f->tail==NULL){
             f->head=n;
@@ -68,14 +83,27 @@ node *defiler(file *f){
     f->head=f->head->next;
     return tmp;
 }
-node *defiler_t(file *f){
+node *defiler_TP(file *f){
     node *tmp=f->head;
-    if (tmp->tmp_exe>0){
+    tmp->etat="A";
+    if (f->head->tmp_exe-3<0){
     f->head=f->head->next;
-    f->tail->next=tmp;
-    tmp->next=NULL;
     }
-
+    return tmp;
+}
+void Tdefiler(file *f){
+    node *tmp=f->head;
+    f->head=tmp->next;
+    f->tail->next=tmp;
+    f->tail=tmp;
+    tmp->next=NULL;
+}
+bool findProh(vector<int>& p,int n){
+     for (int i=0;i<p.size();i++){
+        if (p.at(i)==n)
+        return true;
+     }
+     return false;
 }
 
 void check(file *f){
@@ -130,15 +158,56 @@ void etat(file *f){
     cout<<"le temps moyen d'attent est : "<<total_att/count<<" uc"<<endl;
 }
 
+
 void executer(file *f){
     system("CLS");
      etat(f);
 
 }
 
+void etat_tempPartage(file *f){
+    int count=0,i=0;
+    vector<int> proh;
+    int size = sizeof(arr)/sizeof(arr[0]);
+    float total_att=0;
+    time_t time=f->head->tmp_sec;
+    while (f->head){ 
+        node *rem=defiler_TP(f);
+        total_att+=rem->tmp_att;
+            i=0;
+        while (rem->tmp_exe>0 && i<3){
+            arr[rem->getId()].s.push_back("A ");
+            rem->tmp_exe--;
+            if (isFinished(rem))
+            proh.push_back(rem->getId());
+            for (int k=1;k<=nbr_node;k++){
+                if (k!=rem->getId() && !findProh(proh,k))
+                arr[k].s.push_back("P ");
+            }
+            
+            i++;
+        }
+        count++;
+        if (rem->tmp_exe>0)
+        Tdefiler(f);
+    }
+    for (int i=1;i<=nbr_node;i++){
+        for (int j=0;j<arr[i].s.size();j++){
+        cout<<arr[i].s[j];
+        }
+        cout<<endl;
+    }
+    cout<<"le temps moyen d'attent est : "<<total_att/count<<" uc"<<endl;
+}
+
+void executer_TP(file *f){
+    system("CLS");
+     etat_tempPartage(f);
+
+}
 
 file *cree_file(int id){
-    int tmp,nbr_node,i=0;
+    int tmp,i=0;
     time_t t;
     struct tm* c;
     file *f = new file(NULL,NULL);
@@ -162,9 +231,23 @@ file *cree_file(int id){
     /*check(f);*/
 
 }
+void init(){
+    int temp,id=0;bool terminer=false;
+    system("CLS");
+    cout<<"Menu:"<<endl;
+    cout<<"1.Mode Batch"<<endl;
+    cout<<"2.Mode Temp Partager"<<endl;
+    cin>>temp;
+    switch(temp){
+        case 1 : init_batch();
+        break;
+        case 2 : init_TP();
+        break;
+        default : init();
+    }
+} 
 
-
-int init_batch(){
+void init_batch(){
     int temp,id=0;bool terminer=false;
     file *f = NULL;
     while(!terminer){
@@ -185,12 +268,33 @@ int init_batch(){
     }
 }
 }
+void init_TP(){
+    int temp,id=0;bool terminer=false;
+    file *f = NULL;
+    while(!terminer){
+    system("CLS");
+    cout<<"Menu:"<<endl;
+    cout<<"1.inserer un processus"<<endl;
+    cout<<"2.consulter la file d'attente"<<endl;
+    cout<<"3.executer la sequence des processus"<<endl;
+    cin>>temp;
+    switch(temp){
+        case 1 : id++ ;f=cree_file(id);
+        break;
+        case 2 : check(f);
+        break;
+        case 3 : executer_TP(f); terminer=true;
+        break;
+        default : init_batch();
+    }
+}
+}
 
 
 
 int main(){
 
-init_batch();
+init();
 cout<<"terminer !";
 
 
